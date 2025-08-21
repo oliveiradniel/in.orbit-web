42743ec21b9c52827b64aa5517ed2ad6c298eb76
oliveiradniel <kadadniel@gmail.com>
2025-08-21 16:32:59 -0300

    feat: ensure goal title is not sent with only spaces

--- src/app/schemas/CreateGoalSchema.ts
+++ src/app/schemas/CreateGoalSchema.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 
 export const CreateGoalSchema = z.object({
-  title: z.string().min(1, 'Informe a atividade que deseja realizar.'),
+  title: z.string().trim().min(1, 'Informe a atividade que deseja realizar.'),
   desiredWeeklyFrequency: z.coerce.number().min(1).max(7),
 });
 
