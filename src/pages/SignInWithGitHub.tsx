e268880f15fb92a83e264c45fedbf2fea9096439
oliveiradniel <kadadniel@gmail.com>
2025-09-18 20:02:42 -0300

    feat: add page to login with github

---
+++ src/pages/SignInWithGitHub.tsx
@@ -0,0 +1,24 @@
+import githubIcon from '@/assets/icons/github.svg';
+import logo from '@/assets/images/logo-in-orbit.svg';
+
+import { Button } from '@/components/ui/Button';
+
+export function SignInWithGitHub() {
+  return (
+    <div className="flex h-screen w-screen flex-col items-center justify-center gap-8">
+      <img aria-hidden="true" src={logo} alt="" />
+
+      <p className="max-w-80 text-center leading-relaxed text-zinc-300">
+        Conclua suas metas semanais, ganhe experiência e suba de nível!
+      </p>
+
+      <Button
+        type="button"
+        className="bg-white text-black hover:bg-white hover:opacity-80"
+      >
+        <img aria-hidden="true" src={githubIcon} alt="" className="size-6" />
+        Entrar com GitHub
+      </Button>
+    </div>
+  );
+}
