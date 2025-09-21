584742d44a16999f390e8f5d822be60e53c8e8ee
oliveiradniel <kadadniel@gmail.com>
2025-09-20 22:16:04 -0300

    chore: sync config with GITHUB_CLIENT_ID variable

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,7 +1,9 @@
 interface Environment {
   API_URL: string;
+  GITHUB_CLIENT_ID: string;
 }
 
 export const env: Environment = {
   API_URL: import.meta.env.VITE_API_URL!,
+  GITHUB_CLIENT_ID: import.meta.env.VITE_GITHUB_CLIENT_ID!,
 };
