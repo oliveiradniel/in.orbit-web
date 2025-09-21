3887685abb642a127b7ac9ca7c6007e97f83ac86
oliveiradniel <kadadniel@gmail.com>
2025-09-21 10:40:07 -0300

    feat: add GitHub OAuth login link to SignIn page

--- src/pages/SignInWithGitHub.tsx
+++ src/pages/SignInWithGitHub.tsx
@@ -1,9 +1,17 @@
+import { Link } from '@tanstack/react-router';
+
 import githubIcon from '@/assets/icons/github.svg';
 import logo from '@/assets/images/logo-in-orbit.svg';
 
 import { Button } from '@/components/ui/Button';
 
+import { env } from '@/config/env';
+
 export function SignInWithGitHub() {
+  const githubURL = new URL('http://github.com/login/oauth/authorize');
+
+  githubURL.searchParams.set('client_id', env.GITHUB_CLIENT_ID);
+
   return (
     <div className="flex h-screen w-screen flex-col items-center justify-center gap-8">
       <img aria-hidden="true" src={logo} alt="" />
@@ -13,8 +21,10 @@ export function SignInWithGitHub() {
       </p>
 
       <Button type="button" variant="githubLogin">
-        <img aria-hidden="true" src={githubIcon} alt="" className="size-6" />
-        Entrar com GitHub
+        <Link to={githubURL.toString()} className="flex items-center gap-2">
+          <img aria-hidden="true" src={githubIcon} alt="" className="size-6" />
+          Entrar com GitHub
+        </Link>
       </Button>
     </div>
   );
