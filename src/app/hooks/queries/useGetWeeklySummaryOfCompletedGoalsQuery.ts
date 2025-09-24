b0fcb9638f581572e9f470f1bd7cabcb8e4dff79
oliveiradniel <kadadniel@gmail.com>
2025-09-24 12:25:28 -0300

    feat: send weekStartsAt param in weekly summary query

--- src/app/hooks/queries/useGetWeeklySummaryOfCompletedGoalsQuery.ts
+++ src/app/hooks/queries/useGetWeeklySummaryOfCompletedGoalsQuery.ts
@@ -1,15 +1,21 @@
 import { useQuery } from '@tanstack/react-query';
 
+import { useSearch } from '@tanstack/react-router';
+
 import { makeGoalService } from '@/app/factories/makeGoalService';
 import { delay } from '@/utils/delay';
 
 export function useGetWeeklySummaryOfCompletedGoalsQuery() {
+  const { weekStartsAt } = useSearch({ from: '/' });
+
   const goalService = makeGoalService();
 
   async function getWeeklySummaryOfCompletedGoals() {
     await delay(3000);
 
-    return goalService.getWeeklySummaryOfCompletedGoals();
+    return goalService.getWeeklySummaryOfCompletedGoals({
+      params: { weekStartsAt },
+    });
   }
 
   const { data, isLoading, isFetching } = useQuery({
