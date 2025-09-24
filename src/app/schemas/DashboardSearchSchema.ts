bf39ca84b72d5c84f90b165d09422038258da4f4
oliveiradniel <kadadniel@gmail.com>
2025-09-24 11:02:18 -0300

    fix: make weekStartsAt optional in DashboardSearchSchema and remove default value

--- src/app/schemas/DashboardSearchSchema.ts
+++ src/app/schemas/DashboardSearchSchema.ts
@@ -4,8 +4,8 @@ import z from 'zod';
 export const DashboardSearchSchema = z.object({
   weekStartsAt: z
     .string()
+    .optional()
     .transform((date) => {
       return dayjs(date).format('YYYY-MM-DD');
-    })
-    .default(dayjs().format('YYYY-MM-DD')),
+    }),
 });
