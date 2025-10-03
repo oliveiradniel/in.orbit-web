1d5692cd849f673c93cb2a882306cf2113ac8052
oliveiradniel <kadadniel@gmail.com>
2025-10-03 19:03:25 -0300

    feat: create a hook to fetch all goals

---
+++ src/app/hooks/queries/useGetAllGoalsQuery.ts
@@ -0,0 +1,16 @@
+import { useQuery } from '@tanstack/react-query';
+
+import { makeGoalService } from '@/app/factories/makeGoalService';
+
+export function useGetAllGoalsQuery() {
+  const goalsService = makeGoalService();
+
+  const { data } = useQuery({
+    queryKey: ['goals'],
+    queryFn: () => goalsService.getAll(),
+  });
+
+  return {
+    goals: data,
+  };
+}
