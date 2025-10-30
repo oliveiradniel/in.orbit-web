bb034c10d89c1b6d91563a9228be490944641573
oliveiradniel <kadadniel@gmail.com>
2025-10-30 16:00:40 -0300

    feat: show error message when there is error in completing goals

--- src/app/hooks/mutations/useCreateGoalCompletedMutation.ts
+++ src/app/hooks/mutations/useCreateGoalCompletedMutation.ts
@@ -1,18 +1,44 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useSearch } from '@tanstack/react-router';
 
 import { makeGoalCompletedService } from '@/app/factories/makeGoalCompletedService';
 
+import { invalidateQueries } from '@/utils/invalidateQueries';
+
+import { toast } from '@/view/components/ui/Toast';
+
 export function useCreateGoalCompletedMutation() {
+  const queryClient = useQueryClient();
+
+  const { weekStartsAt } = useSearch({ from: '/' });
+
   const goalCompletedService = makeGoalCompletedService();
 
-  const { mutateAsync, isPending } = useMutation({
+  const { mutate, isPending } = useMutation({
     mutationFn: (goalId: string) => {
       return goalCompletedService.create(goalId);
+    },
+    onSuccess: () => {
+      invalidateQueries({
+        queryClient,
+        keys: [
+          ['weeklySummary', weekStartsAt],
+          ['weeklyGoals'],
+          ['userLevel'],
+          ['totalQuantityOfGoalsCompleted'],
+        ],
+      });
+    },
+    onError: () => {
+      toast({
+        description: 'Não foi possível completar a meta.',
+        type: 'error',
+      });
     },
   });
 
   return {
-    createGoalCompleted: mutateAsync,
+    createGoalCompleted: mutate,
     isCreatingGoalCompleted: isPending,
   };
 }
