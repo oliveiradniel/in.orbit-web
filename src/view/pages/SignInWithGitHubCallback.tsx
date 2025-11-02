4d33bb2df85ebbeb51557fc898ea54c4c2074cff
oliveiradniel <kadadniel@gmail.com>
2025-11-02 14:35:47 -0300

    refactor: change the login loading design using GitHub

--- src/view/pages/SignInWithGitHubCallback.tsx
+++ src/view/pages/SignInWithGitHubCallback.tsx
@@ -2,6 +2,8 @@ import { Loader2 } from 'lucide-react';
 
 import { useEffect } from 'react';
 
+import githubInner from '@/assets/icons/github-inner.svg';
+
 import { useOAuthMutation } from '@/app/hooks/mutations/useOAuthMutation';
 
 interface SignInWithGitHubCallbackParams {
@@ -20,7 +22,20 @@ export function SignInWithGitHubCallback({
   return (
     <div className="flex h-screen items-center justify-center">
       {isAuthenticating && (
-        <Loader2 className="size-8 animate-spin text-gray-500" />
+        <div className="space-y-6">
+          <img
+            src={githubInner}
+            alt=""
+            width={200}
+            height={200}
+            className="opacity-80"
+          />
+
+          <div className="flex items-center gap-2">
+            <Loader2 className="size-8 animate-spin text-gray-500" />
+            Autenticando com GitHub
+          </div>
+        </div>
       )}
     </div>
   );
